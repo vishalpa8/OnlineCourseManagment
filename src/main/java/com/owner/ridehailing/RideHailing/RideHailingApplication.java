09bfe9e970412da3cd0218ec9160c9481d1ae777
Satya Prakash Pandey <satya.pandey@qualitestgroup.com>
2025-09-25 16:04:38 +0530

    pro

---
+++ src/main/java/com/owner/ridehailing/RideHailing/RideHailingApplication.java
@@ -0,0 +1,13 @@
+package com.owner.ridehailing.RideHailing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RideHailingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RideHailingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/owner/ridehailing/RideHailing/RideHailingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.owner.ridehailing.RideHailing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RideHailingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
